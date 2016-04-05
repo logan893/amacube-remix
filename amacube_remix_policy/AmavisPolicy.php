4bcfac86cd8d8d3423e1227d86612564154f7efb
logan893 <logan893@users.noreply.github.com>
2016-04-05 15:20:53 +0200

    Update AmavisPolicy.php

--- amacube_remix_policy/AmavisPolicy.php
+++ amacube_remix_policy/AmavisPolicy.php
@@ -2,31 +2,33 @@
 /**
 * This file is part of the Amacube-Remix_Policy Roundcube plugin
 * Copyright (C) 2015, Tony VanGerpen <Tony.VanGerpen@hotmail.com>
-* 
+*
 * A Roundcube plugin to let users change their amavis policy settings (which must be stored in a database)
 * Based heavily on the amacube plugin by Alexander Köb (https://github.com/akoeb/amacube)
-* 
-* Licensed under the GNU General Public License version 3. 
+*
+* Licensed under the GNU General Public License version 3.
 * See the COPYING file in parent directory for a full license statement.
 */
 
 class AmavisPolicy
 {
-	# Database Settings
-	private   $db_config;
+        # Database Settings
+        private   $db_config;
   protected $db_conn;
-	
+
   # User Settings
   private $priority = 7; // we do not change the amavis default for that
   protected $user_email = '';
-	public $user_pk; // primary key for the user record
-  public $fullname; // Full Name of the user, for reference, Amavis does not use that  
+        public $user_pk; // primary key for the user record
+  public $fullname; // Full Name of the user, for reference, Amavis does not use that
 
   # Policy Settings
   public $policy_pk; // primary key of the policy record
   public $policy_name; // Name of the policy, for reference, Amavis does not use that
   public $policy_settings = array();
-  
+  public $policy_settings_default = array();
+  public $policy_access = array();
+
   // class variables(static), the same in all instances:
   private static $boolean_settings = array(
     'virus_lover',
@@ -39,7 +41,7 @@ class AmavisPolicy
     'bypass_banned_checks',
     'bypass_header_checks'
     );
-  
+
   // class variables(static), the same in all instances:
   private static $quarantines = array(
     'virus_quarantine_to',
@@ -50,30 +52,50 @@ class AmavisPolicy
     'clean_quarantine_to',
     'archive_quarantine_to'
     );
-  
-  function __construct( $db_config, $default_policy ) {
+
+  private static $policy_access_map = array(
+    'virus_lover' => 'allow_virus_settings',
+    'bypass_virus_checks' => 'allow_virus_settings',
+    'virus_quarantine_to' => array('allow_virus_settings','allow_virus_setting_destination'),
+
+    'spam_lover' => 'allow_spam_settings',
+    'bypass_spam_checks' => 'allow_spam_settings',
+    'spam_quarantine_to' => array('allow_spam_settings','allow_spam_settings_destination'),
+
+    'banned_files_lover' => 'allow_banned_files_settings',
+    'bypass_banned_checks' => 'allow_banned_files_settings',
+    'banned_quarantine_to' => array('allow_banned_files_settings','allow_banned_files_settings_destination'),
+
+    'bad_header_lover' => 'allow_bad_header_settings',
+    'bypass_header_checks' => 'allow_bad_header_settings',
+    'bad_header_quarantine_to' => array('allow_bad_header_settings','allow_bad_header_settings_destination')
+    );
+
+  function __construct( $db_config, $default_policy, $default_policy_access ) {
     $this->db_config = $db_config;
-    
-		# Fetch Username
-		$rcmail = rcmail::get_instance();
+
+                # Fetch Username
+                $rcmail = rcmail::get_instance();
     $this->user_email = $rcmail->user->data['username'];
-    
+
     # Default Policy Settings
+    $this->policy_settings_default = $default_policy;
     $this->policy_settings = $default_policy;
-    
+    $this->policy_access = $default_policy_access;
+
     # Read User's Policy from db if it exists
     $this->read_from_db();
-    
+
     # Verify User's Policy
     $verify = $this->verify_policy_array();
-				
+
     if( isset( $verify ) && is_array( $verify ) ) {
         // TODO: something is dead wrong, database settngs do not verify
         // FiXME: throw error
         error_log("AMACUBE: verification of database settings failed...".implode(',',$verify));
     }
   }
-  
+
   function init_db() {
     # Initialize Database Factory
     if ( !$this->db_conn ) {
@@ -82,7 +104,7 @@ class AmavisPolicy
       else
         $this->db_conn = rcube_db::factory( $this->db_config, '', TRUE ); // ver 0.9+
     }
-    
+
     # Connect to the Database
     $this->db_conn->db_connect('w');
 
@@ -99,33 +121,33 @@ class AmavisPolicy
     # Return the last database error message
     if( $this->db_conn && $this->db_conn->is_error() )
       return $this->db_conn->is_error();
-      
+
     return false;
   }
-    
+
   // method to verify the policy settings are correct
   function verify_policy_array( $array = null ) {
     $errors = array();
-    
+
     # Load Default policty if array argument is empty
     if( !is_array( $array ) || count( $array ) == 0 )
       $array = $this->policy_settings;
-      
+
     # Validate Booleans
-    if( !is_bool( $array['virus_lover'] ) )								array_push( $errors, 'virus_lover' );
-    if( !is_bool( $array['spam_lover'] ) )								array_push( $errors, 'spam_lover' );
-    if( !is_bool( $array['unchecked_lover'] ) )						array_push( $errors, 'unchecked_lover' );
-    if( !is_bool( $array['banned_files_lover'] ) )				array_push( $errors, 'banned_files_lover' );
-    if( !is_bool( $array['bad_header_lover'] ) )					array_push( $errors, 'bad_header_lover' );
-    if( !is_bool( $array['bypass_virus_checks'] ) )				array_push( $errors, 'bypass_virus_checks' );
-    if( !is_bool( $array['bypass_spam_checks'] ) )				array_push( $errors, 'bypass_spam_checks' );
-    if( !is_bool( $array['bypass_banned_checks'] ) )			array_push( $errors, 'bypass_banned_checks' );
-    if( !is_bool( $array['bypass_header_checks'] ) ) 			array_push( $errors, 'bypass_header_checks' );
-    if( !is_bool( $array['virus_quarantine_to'] ) )				array_push( $errors, 'virus_quarantine_to' );
-    if( !is_bool( $array['spam_quarantine_to'] ) )				array_push( $errors, 'spam_quarantine_to' );
-    if( !is_bool( $array['banned_quarantine_to'] ) ) 			array_push( $errors, 'banned_quarantine_to' );
+    if( !is_bool( $array['virus_lover'] ) )                                                             array_push( $errors, 'virus_lover' );
+    if( !is_bool( $array['spam_lover'] ) )                                                              array_push( $errors, 'spam_lover' );
+    if( !is_bool( $array['unchecked_lover'] ) )                                         array_push( $errors, 'unchecked_lover' );
+    if( !is_bool( $array['banned_files_lover'] ) )                              array_push( $errors, 'banned_files_lover' );
+    if( !is_bool( $array['bad_header_lover'] ) )                                        array_push( $errors, 'bad_header_lover' );
+    if( !is_bool( $array['bypass_virus_checks'] ) )                             array_push( $errors, 'bypass_virus_checks' );
+    if( !is_bool( $array['bypass_spam_checks'] ) )                              array_push( $errors, 'bypass_spam_checks' );
+    if( !is_bool( $array['bypass_banned_checks'] ) )                    array_push( $errors, 'bypass_banned_checks' );
+    if( !is_bool( $array['bypass_header_checks'] ) )                    array_push( $errors, 'bypass_header_checks' );
+    if( !is_bool( $array['virus_quarantine_to'] ) )                             array_push( $errors, 'virus_quarantine_to' );
+    if( !is_bool( $array['spam_quarantine_to'] ) )                              array_push( $errors, 'spam_quarantine_to' );
+    if( !is_bool( $array['banned_quarantine_to'] ) )                    array_push( $errors, 'banned_quarantine_to' );
     if( !is_bool( $array['unchecked_quarantine_to'] ) )   array_push( $errors, 'unchecked_quarantine_to' );
-    if( !is_bool( $array['bad_header_quarantine_to'] ) )	array_push( $errors, 'bad_header_quarantine_to' );
+    if( !is_bool( $array['bad_header_quarantine_to'] ) )        array_push( $errors, 'bad_header_quarantine_to' );
     if( !is_bool( $array['clean_quarantine_to'] ) )       array_push( $errors, 'clean_quarantine_to' );
     if( !is_bool( $array['archive_quarantine_to'] ) )     array_push( $errors, 'archive_quarantine_to' );
 
@@ -136,13 +158,13 @@ class AmavisPolicy
     if( !is_numeric( $array['spam_kill_level'] ) ) array_push($errors, 'spam_kill_level');
     if( !is_numeric( $array['spam_dsn_cutoff_level'] ) ) array_push( $errors, 'spam_dsn_cutoff_level' );
     if( !is_numeric( $array['spam_quarantine_cutoff_level'] ) ) array_push($errors, 'spam_quarantine_cutoff_level');
-    
+
     # Whitelist: Strip Extra Elements from Policy
     foreach( $array as $key => $value ) {
       if( !array_key_exists( $key, $this->policy_settings ) )
         array_push( $errors, 'unknown: ' . $key );
     }
-		
+
     # Return Errors
     if( !empty( $errors ) )
       return $errors;
@@ -157,7 +179,7 @@ class AmavisPolicy
     # Build Query
     $query = 'SELECT users.id as user_id, users.priority, users.email, users.fullname, policy.*
       FROM users, policy
-      WHERE users.policy_id = policy.id 
+      WHERE users.policy_id = policy.id
       AND users.email = ? ';
 
     # Execute Query
@@ -167,31 +189,31 @@ class AmavisPolicy
     // write the first result line to settings array
     if( $results ) {
       if( $results_array = $this->db_conn->fetch_assoc( $results ) ) {
-        
+
         // read all keys of policy_settings array
         foreach( $this->policy_settings as $key => $value )
           $this->policy_settings[$key] = $this->map_from_db( $key, $results_array[$key] );
-          
+
         $this->user_pk = $results_array['user_id'];
         $this->priority = $results_array['priority'];
         $this->fullname = $results_array['fullname'];
         $this->policy_pk = $results_array['id'];
         $this->policy_name = $results_array['policy_name'];
-        
+
         return;
       }
     }
-    
+
     //TODO: Show Error: Unable to read policy from the database
   }
-  
+
   // write settings back to database
   // FIXME: this method must return an error string in case something fails
   function write_to_db() {
     # Connect to db
     if( !is_resource( $this->db_conn ) )
       $this->init_db();
-          
+
     $query_params = array();
 
     // TODO: Remove before production
@@ -203,7 +225,7 @@ class AmavisPolicy
       $query = 'UPDATE policy SET ';
       $keys = array_keys( $this->policy_settings );
       $max = sizeof( $keys );
-      
+
       for( $i = 0; $i < $max; $i++ ) {
         $query .= $keys[$i] .' = ? ';
         array_push($query_params, $this->map_to_db($keys[$i], $this->policy_settings[$keys[$i]]));
@@ -239,18 +261,18 @@ class AmavisPolicy
             }
         }
     }
-    
-				
+
+
       $res = $this->db_conn->query($query, $query_params);
-						
+
       //print_r($res);exit;
-      
+
       // error check
       if($this->db_error()) {
           return "Error in insert/update policy: ".$this->db_error();
       }
 
-      // in case this was an insert, read policy_pk and insert user as well if needed 
+      // in case this was an insert, read policy_pk and insert user as well if needed
       if(empty($this->policy_pk)) {
 
           $this->policy_pk = $this->db_conn->insert_id();
@@ -259,12 +281,12 @@ class AmavisPolicy
               return "Could not get Primary Key for policy: ".$this->db_error();
           }
 
-          // now that we have the policy pk, we check 
+          // now that we have the policy pk, we check
           // whether we need to insert or update the user as well
           $res = $this->db_conn->query(
-              'SELECT id from users where email = ? ', 
+              'SELECT id from users where email = ? ',
               $this->user_email);
-          
+
           // error check
           if($this->db_error) {
               return "Error in checking for user record: ".$this->db_error();
@@ -294,7 +316,7 @@ class AmavisPolicy
 
 
   // CONVENIENCE METHODS:
-  
+
   // set the checkbox checked mark if user is a NOT spam or virus lover
   // (the checkbox marks ACTIVATION of the check, DEACTIVATION means user is a *_lover)
   function is_check_activated_checkbox( $type ) {
@@ -305,10 +327,10 @@ class AmavisPolicy
     elseif( $this->policy_settings['bypass_'.$type.'_checks'] ) {
       return false;
     }
-    
+
     return true;
   }
-    
+
   function is_lover_activated_checkbox( $type ) {
     if( $type !== 'banned_files' && $type !== 'bad_header' && $type !== 'spam' && $type !== 'virus' ) {
         //FIXME throw error
@@ -317,10 +339,10 @@ class AmavisPolicy
     elseif ( $this->policy_settings[$type.'_lover'] ) {
       return false;
     }
-    
+
     return true;
   }
-    
+
   // set the checkbox checked mark if user has quarantine activated
   function is_quarantine_activated_checkbox( $type ) {
     if( $type !== 'banned' && $type !== 'bad_header' && $type !== 'spam' && $type !== 'virus' ) {
@@ -330,13 +352,39 @@ class AmavisPolicy
     elseif($this->policy_settings[$type.'_quarantine_to']) {
       return true;
     }
-    
+
     return false;
+  }
+
+  // check policy access, return true as default
+  function get_policy_access( $policy_access_list ) {
+    $retval = true;
+    // wrap non-array values in array
+    if ( !is_array( $policy_access_list ) ) $policy_access_list = array($policy_access_list);
+    foreach ( $policy_access_list as $policy_access_name ) {
+      // deny policy if any mapped item is false
+      if ( array_key_exists( $policy_access_name, $this->policy_access ) && $this->policy_access[$policy_access_name] === false ) {
+        $retval = false;
+        break;
+      }
+    }
+    return $retval;
+  }
+
+  function apply_policy_access_map( $key, $value ) {
+    // only process key which maps to policy access
+    if ( array_key_exists( $key, self::$policy_access_map ) && $this->get_policy_access( self::$policy_access_map[$key] ) === false )
+      return $this->policy_settings_default[$key];
+    else
+      return $value;
   }
 
   // mapping function internal representation - database content
   function map_to_db( $key, $value ) {
     $retval = '';
+
+    // use defauly value if policy prevents user-specific settings
+    $value = $this->apply_policy_access_map( $key, $value );
 
     # Map boolean settings to Y/N as stored in the database
     if( in_array( $key, self::$boolean_settings ) ) {
@@ -345,7 +393,7 @@ class AmavisPolicy
       else
         $retval = 'N';
     }
-    
+
     # special mapping for the quarantine settings we use:
     elseif( in_array( $key, self::$quarantines ) ) {
       if( $value )
@@ -353,19 +401,19 @@ class AmavisPolicy
       else
         $retval = '';
     }
-    
+
     # all other settings do not require mapping
     else {
       $retval = $value;
     }
-    
+
     return $retval;
   }
-  
-  // mapping function database content - internal representation 
+
+  // mapping function database content - internal representation
   function map_from_db( $key, $value ) {
     $retval = $value;
-    
+
     # Map boolean settings from Y/N as stored in the database
     if( in_array( $key, self::$boolean_settings ) ) {
       if( !empty( $value ) && $value == 'Y' )
@@ -373,7 +421,7 @@ class AmavisPolicy
       else
         $retval = false;
     }
-    
+
     # special mapping for the quarantine settings we use:
     elseif( in_array( $key, self::$quarantines ) ) {
       if( !empty( $value ) && $value == 'sql:' )
@@ -381,7 +429,10 @@ class AmavisPolicy
       else
         $retval = false;
     }
-    
+
+    // use defauly value if policy prevents user-specific settings
+    $retval = $this->apply_policy_access_map( $key, $retval );
+
     return $retval;
   }
 }
